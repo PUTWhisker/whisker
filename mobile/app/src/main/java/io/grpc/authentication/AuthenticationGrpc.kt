472ed86c0387479687fc0241c6f93f8969f87f91
Krybczynski <krybczynski12@gmail.com>
2024-09-28 15:32:48 +0200

    feat: implement authentication service

--- mobile/app/src/main/java/io/grpc/authentication/AuthenticationGrpc.kt
+++ mobile/app/src/main/java/io/grpc/authentication/AuthenticationGrpc.kt
@@ -2,12 +2,13 @@ package io.grpc.authentication
 
 import android.net.Uri
 import io.grpc.ManagedChannelBuilder
-import io.grpc.soundtransfer.SoundServiceGrpcKt
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asExecutor
 import java.io.Closeable
+import io.grpc.Metadata
 
 class AuthenticationGrpc(uri: Uri) : Closeable {
+    private var JWT = ""
     private val channel = let {
         println("Connecting to ${uri.host}:${uri.port}")
 
@@ -20,8 +21,9 @@ class AuthenticationGrpc(uri: Uri) : Closeable {
 
         builder.executor(Dispatchers.IO.asExecutor()).build()
     }
-    private  val transferer = SoundServiceGrpcKt.SoundServiceCoroutineStub(channel)
+//    private  val transferer = SoundServiceGrpcKt.SoundServiceCoroutineStub(channel)
 
+    private  val transfer = ClientServiceGrpcKt.ClientServiceCoroutineStub(channel)
 
     override fun close() {
         channel.shutdownNow()
@@ -29,10 +31,31 @@ class AuthenticationGrpc(uri: Uri) : Closeable {
 
 
 
-//    suspend fun Login(user : UserCredits)  = LoginResponse{
-//        JWT = "dwa",
-//        successful = true
-//    }
+    suspend fun Login(user : String, password : String) : Boolean {
+        val arg = userCredits { this.username = user; this.password = password }
+        val response = transfer.login(arg)
+
+        if (response.successful) {
+            this.JWT = response.jwt
+            return true
+        }
+        return false
+    }
+
+
+    suspend fun Register(user : String, password : String) :Boolean {
+        val arg = userCredits { this.username = user; this.password = password }
+        val response = transfer.register(arg)
+        return response.successful
+    }
+
+    suspend fun GetTranslations() {
+        val metadata = Metadata()
+        val key = Metadata.Key.of("JWT", Metadata.ASCII_STRING_MARSHALLER)
+        metadata.put(key, this.JWT)
+        val arg = empty {  }
+        val response = transfer.getTranslation(empty {  }, metadata)
+    }
 }
 
 
