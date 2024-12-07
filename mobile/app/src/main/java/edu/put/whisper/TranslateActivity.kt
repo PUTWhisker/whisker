a5639a4fc36c83c2e8c6e38460a8c270bfa7b911
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-12-07 18:50:21 +0100

    add basic version of translation with few languages

--- mobile/app/src/main/java/edu/put/whisper/TranslateActivity.kt
+++ mobile/app/src/main/java/edu/put/whisper/TranslateActivity.kt
@@ -1,12 +1,93 @@
 package edu.put.whisper
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import io.grpc.soundtransfer.SoundTransferClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
 
 class TranslateActivity : AppCompatActivity() {
+
+    private lateinit var client: SoundTransferClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
 
-}
+        val serverUrl = getString(R.string.server_url)
+        val serverUri = Uri.parse(serverUrl)
+        client = SoundTransferClient(serverUri)
+        val transcriptionText = intent.getStringExtra("EXTRA_TRANSCRIPTION_TEXT")
+
+        val spinnerLanguage = findViewById<Spinner>(R.id.spinner_language)
+        val spinnerTargetLanguage = findViewById<Spinner>(R.id.spinner_target_language)
+        val editTextOriginal = findViewById<EditText>(R.id.edit_text_original)
+        val editTextTranslated = findViewById<EditText>(R.id.edit_text_translated)
+        val buttonTranslate = findViewById<Button>(R.id.button_translate)
+
+        val languages = resources.getStringArray(R.array.languages)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerLanguage.adapter = adapter
+        spinnerTargetLanguage.adapter = adapter
+
+        //domyslne wartosci wybranych jezykow
+        spinnerLanguage.setSelection(languages.indexOf("pl"))
+        spinnerTargetLanguage.setSelection(languages.indexOf("en"))
+
+        if (!transcriptionText.isNullOrEmpty()) {
+            editTextOriginal.setText(transcriptionText)
+        }
+
+
+        buttonTranslate.setOnClickListener {
+            val originalText = editTextOriginal.text.toString()
+            val sourceLanguage = spinnerLanguage.selectedItem.toString()
+            val targetLanguage = spinnerTargetLanguage.selectedItem.toString()
+
+            if (originalText.isNotEmpty()) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    try {
+                        val tempFile = createTempFile(originalText)
+                        val flow = client.translate(tempFile.absolutePath, sourceLanguage, targetLanguage)
+
+                        var translatedText = ""
+                        flow.collect { response ->
+                            translatedText += response.text
+                        }
+
+
+                        editTextTranslated.setText(translatedText)
+
+                        tempFile.delete()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        editTextTranslated.setText("Wystąpił błąd podczas tłumaczenia")
+                    }
+                }
+            } else {
+                editTextTranslated.setText("Wprowadź tekst do przetłumaczenia")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        client.close()
+    }
+
+    private fun createTempFile(text: String): File {
+        val tempFile = File.createTempFile("soundtransfer", ".txt", cacheDir)
+        tempFile.writeText(text)
+        return tempFile
+    }
+
 }
